7678d3b3672a07ec02d4f046e4edd489f3a89c7c
stacejsk <tusheestaycee@gmail.com>
2024-04-04 17:42:50 +0300

    Added Object Oriented program

---
+++ mycalculator/bin/mycalculator.dart
@@ -0,0 +1,5 @@
+import 'package:mycalculator/mycalculator.dart' as mycalculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mycalculator.calculate()}!');
+}
---
+++ mycalculator/test/mycalculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:mycalculator/mycalculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
